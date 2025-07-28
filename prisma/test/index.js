8d8cc6134e7582674fb6f88c813249660a169c54
aloe <120464393+aloe-lue@users.noreply.github.com>
2025-07-28 09:41:00 +0800

    feat: export seeder functions send it to seed dir

---
+++ prisma/test/index.js
@@ -0,0 +1,11 @@
+import randMember from "./member.js";
+import randAuthor from "./author.js";
+import randPost from "./post.js";
+import randCommentOnPosts from "./comment.js";
+
+export default {
+  randMember,
+  randAuthor,
+  randPost,
+  randCommentOnPosts,
+};
