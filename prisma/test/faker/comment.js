2126c0ceae6ec1d5efa8841fc9f84b0ba0538ddd
aloe <120464393+aloe-lue@users.noreply.github.com>
2025-07-28 09:37:18 +0800

    feat: use faker to create a function that adds comment on posts

--- prisma/test/faker/comment.js
+++ prisma/test/faker/comment.js
@@ -2,16 +2,36 @@ import { faker } from "@faker-js/faker";
 
 const { string, book, date, lorem } = faker;
 
-const comment = function getRandComment() {
+/**
+ *
+ * @param {*} userId
+ * @param {*} postId
+ * @returns
+ */
+const comment = function getRandComment(userId, postId) {
   return {
     id: string.uuid(),
     title: book.title(),
     content: lorem.paragraph(),
     dateCreated: date.recent(),
     dateUpdated: date.anytime(),
-    userId: string.uuid(),
-    postId: string.uuid(),
+    userId: userId,
+    postId: postId,
   };
 };
 
-export default comment;
+/**
+ *
+ * @param {*} userIds
+ * @param {*} postId
+ */
+const commentsOnPost = function getRandCommentsOnPost(userIds, postId) {
+  // do this when you want comments on post;
+};
+
+// comment on many posts
+const commentOnPosts = function getRandCommentOnPosts(userId, postIds) {
+  return postIds.map(({ id }) => comment(userId, id));
+};
+
+export default commentOnPosts;
