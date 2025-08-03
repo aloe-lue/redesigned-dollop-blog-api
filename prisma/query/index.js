dd7bcfada99854be2de10ba5f6ebc4ca342a0a70
aloe <120464393+aloe-lue@users.noreply.github.com>
2025-08-03 15:55:12 +0800

    feat: export all queries

---
+++ prisma/query/index.js
@@ -0,0 +1,17 @@
+import user from "./user.js";
+import member from "./member.js";
+import author from "./author.js";
+import profile from "./profile.js";
+import picture from "./picture.js";
+import post from "./post.js";
+import comment from "./comment.js";
+
+export default {
+  user,
+  member,
+  author,
+  profile,
+  picture,
+  post,
+  comment,
+};
