c8195b072dbd3f554405183d8c6a0b66874c3698
aloe <120464393+aloe-lue@users.noreply.github.com>
2025-08-18 17:35:38 +0800

    feat: you want to import all controllers at once

---
+++ controllers/indexController.js
@@ -0,0 +1,9 @@
+import user from "./userController.js";
+import posts from "./postsController.js";
+import comment from "./commentController.js";
+
+export default {
+  user,
+  posts,
+  comment,
+};
