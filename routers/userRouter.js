7945d60ddc250ac14b261abab6dcd3bdfdeb283d
aloe <120464393+aloe-lue@users.noreply.github.com>
2025-08-19 15:42:15 +0800

    feat: added log out and account deletion routes

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,22 +1,12 @@
 import { Router } from "express";
 import controller from "../controllers/indexController.js";
-const {
-  postUserMember,
-  postUserMemberAuth,
-  postUserAuthor,
-  postUserAuthorAuth,
-} = controller.user;
+const { setUser, getUserToken, logOutUser, deleteUser } = controller.user;
 
 const userRouter = Router();
-// create user : member
-userRouter.post("/sign_up", postUserMember);
-userRouter.post("/log_in", postUserMemberAuth);
-
-// create user : author
-userRouter.post("/sign_up/author", postUserAuthor);
-userRouter.post("/log_in/author", postUserAuthorAuth);
-
-// create a log out that removes the token from the req.headers
-// userRouter.get("/log_out", );
+// create user
+userRouter.post("/sign_up", setUser);
+userRouter.post("/log_in", getUserToken);
+userRouter.get("/log_out", logOutUser);
+userRouter.delete("/delete_user", deleteUser);
 
 export default userRouter;
