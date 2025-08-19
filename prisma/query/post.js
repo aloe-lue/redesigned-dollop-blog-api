104a22880d7d602c357277d2287bd6af0fd49613
aloe <120464393+aloe-lue@users.noreply.github.com>
2025-08-19 15:34:22 +0800

    fix: added isPublished on add post to be false
    fix: transaction should not contain await inside
    fix: return data structure that response to model

--- prisma/query/post.js
+++ prisma/query/post.js
@@ -1,8 +1,17 @@
 import prisma from "../client/prismaClient.js";
+import comment from "./comment.js";
 
 class Post {
   addPost = async (data) => {
-    const { id, title, content, dateCreated, dateUpdated, authorId } = data;
+    const {
+      id,
+      title,
+      content,
+      dateCreated,
+      dateUpdated,
+      authorId,
+      isPublished,
+    } = data;
     await prisma.post.create({
       data: {
         id,
@@ -11,20 +20,60 @@ class Post {
         dateCreated,
         dateUpdated,
         authorId,
+        isPublished,
       },
     });
   };
 
+  getPostIdsByAuthorId = async (authorId) => {
+    const data = await prisma.post.findMany({
+      where: {
+        authorId,
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    return data;
+  };
+
   getPosts = async () => {
     const data = await prisma.post.findMany({
+      take: 10,
       select: {
         id: true,
         title: true,
         dateCreated: true,
         dateUpdated: true,
-        author: true,
+        author: false,
         authorId: true,
         comments: false,
+      },
+      where: {
+        isPublished: true,
+      },
+    });
+
+    return data;
+  };
+
+  getPostsPage = async (skip = 1) => {
+    const data = await prisma.post.findMany({
+      take: 10,
+      skip: 10 * skip,
+      select: {
+        id: true,
+        title: true,
+        dateCreated: true,
+        dateUpdated: true,
+        author: false,
+        authorId: true,
+        comments: false,
+      },
+
+      where: {
+        isPublished: true,
       },
     });
 
@@ -42,21 +91,38 @@ class Post {
         title: true,
         dateCreated: true,
         dateUpdated: true,
-        author: true,
-        authorId: true,
-        comments: true,
+        author: {
+          select: {
+            id: true,
+            userId: true,
+          },
+        },
+        // don't include all comments at all once
+        comments: false,
       },
     });
-
     return data;
   };
 
-  deletePost = async (authorId) => {
-    await prisma.post.delete({
+  deletePost = async (id) => {
+    await prisma.$transaction([
+      comment.deletePostComments(id),
+      prisma.post.delete({
+        where: {
+          id,
+        },
+      }),
+    ]);
+  };
+
+  getPostByIdAndTitle = async (id, title) => {
+    const data = await prisma.post.findUnique({
       where: {
-        authorId,
+        id,
+        title,
       },
     });
+    return data;
   };
 
   // useful when deleting author account
@@ -70,6 +136,22 @@ class Post {
 
   updatePost = async (data) => {
     const { id, title, content, dateUpdated } = data;
+    let { isPublished = "false" } = data;
+
+    let isPublishedBoolean = true;
+    if (typeof isPublished !== "boolean") {
+      isPublishedBoolean = false;
+    }
+
+    const isPublishedBooleanColl = {
+      false: false,
+      true: true,
+    };
+
+    if (!isPublishedBoolean) {
+      isPublished = isPublishedBooleanColl[isPublished];
+    }
+
     await prisma.post.update({
       where: {
         id,
@@ -78,6 +160,7 @@ class Post {
         title,
         content,
         dateUpdated,
+        isPublished,
       },
     });
   };
