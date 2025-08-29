424e5248a9e723d103fea2cda885fea186a51e11
aloe <120464393+aloe-lue@users.noreply.github.com>
2025-08-29 10:55:22 +0800

    Get user id by decoding jwt token
    this is useful for loading user data in the frontend
    use the userId to fetch post data from the db

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import controller from "../controllers/indexController.js";
-const { setUser, getUserToken, logOutUser, deleteUser } = controller.user;
+const { setUser, getUserToken, logOutUser, deleteUser, getUserByToken } =
+  controller.user;
 
 const userRouter = Router();
 // create user
+userRouter.get("/:token", getUserByToken);
 userRouter.post("/sign_up", setUser);
 userRouter.post("/log_in", getUserToken);
 userRouter.get("/log_out", logOutUser);
